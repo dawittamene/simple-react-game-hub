847a5b53721313932a7965362b3a32fad32e0bbb
dawittamene <dawittamene559@gmail.com>
2023-12-04 00:51:20 +0300

    show loading skleton

--- game/src/components/GameCard.tsx
+++ game/src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react"
+import { Card, CardBody, HStack, Heading, Image, } from "@chakra-ui/react"
 import { Game } from "../hooks/useGames"
 import PlatformIconList from "./PlatformIconList"
 import CriticScore from "./CriticScore"
@@ -11,7 +11,7 @@ interface props{
 
 const GameCard = ({game}:props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card width='300px' borderRadius={10} overflow="hidden">
         <Image src={getCroppedImageUrl(game.background_image)}/>
         <CardBody>
           <Heading fontSize='2xl'>{game.name}</Heading>
