9592211ab23af557984ca0daac724c1853d6f4d3
dawittamene <dawittamene559@gmail.com>
2023-12-04 01:03:18 +0300

    Remove duplicated styles

--- game/src/components/GameGride.tsx
+++ game/src/components/GameGride.tsx
@@ -2,6 +2,7 @@ import { SimpleGrid } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 
 const GameGride = () => {
@@ -12,9 +13,11 @@ const GameGride = () => {
     <>
       {error && <p>{error}</p>}
       <SimpleGrid columns={{sm:1, md:2, lg:3, xl:5 }} padding='10px' spacing={10}>
-        {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton}/>)}
+        {isLoading && skeletons.map(skeleton => <GameCardContainer><GameCardSkeleton key={skeleton}/></GameCardContainer>)}
         {games.map((game) => (
+          <GameCardContainer>
           <GameCard key={game.id} game={game} />
+          </GameCardContainer>
           
         ))}
       </SimpleGrid>
