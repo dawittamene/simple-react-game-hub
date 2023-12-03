2ee2c243071ba0aefb3edb99684093f3e5adbd53
dawittamene <dawittamene559@gmail.com>
2023-12-04 02:49:09 +0300

    Display genres

--- game/src/components/GameGride.tsx
+++ game/src/components/GameGride.tsx
@@ -12,7 +12,7 @@ const GameGride = () => {
   return (
     <>
       {error && <p>{error}</p>}
-      <SimpleGrid columns={{sm:1, md:2, lg:3, xl:5 }} padding='10px' spacing={10}>
+      <SimpleGrid columns={{sm:1, md:2, lg:3, xl:5 }} padding='10px' spacing={3}>
         {isLoading && skeletons.map(skeleton => <GameCardContainer><GameCardSkeleton key={skeleton}/></GameCardContainer>)}
         {data.map((game) => (
           <GameCardContainer>
